ae30e9f99ab9656cfdc8c3e39b158e3b26d4275d
agusmena <agusmena60@mail.com>
2024-03-06 14:21:00 -0300

    add changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import cors from "cors";
 import './database';
 import router from './routes/property.routes';
+import 'dotenv/config'
 
 const app = express()
 
--- src/middlewares/propertyValidation.js
+++ src/middlewares/propertyValidation.js
@@ -1,22 +1,22 @@
-import { check,  } from "express-validator";
+import { check } from "express-validator";
 import validationresults from "../helpers/validationResults";
 
-const propertyValidation =[
-    check('typeProperty')
+const propertyValidation = [
+  check("typeProperty")
     .notEmpty()
-    .withMessage('El tipo de propiedad es obligatorio'),
-    check('typeTransaction')
+    .withMessage("El tipo de propiedad es obligatorio"),
+  check("typeTransaction")
     .notEmpty()
-    .withMessage('El tipo de transaccion es obligatoria'),
-    check('location')
-    .notEmpty()
-    .withMessage('La ubicacion es obligatoria')
-    ,
-    check('description')
-    .withMessage('La descripcion es obligatoria'),
-    (req, res, next) => {
-        validationresults(req, res, next);
-      },
-]
+    .withMessage("El tipo de transaccion es obligatoria"),
+  check("location")
+  .notEmpty()
+  .withMessage("La ubicacion es obligatoria"),
+  check("description")
+  .notEmpty()
+  .withMessage("La descripcion es obligatoria"),
+  (req, res, next) => {
+    validationresults(req, res, next);
+  },
+];
 
 export default propertyValidation;
