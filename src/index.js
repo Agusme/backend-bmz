24e927b4cf9c9359a1f4e6ccda3bc122ef53d631
agusmena <agusmena60@mail.com>
2024-03-06 14:52:42 -0300

    add

--- src/index.js
+++ src/index.js
@@ -16,6 +16,6 @@ app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.use('/api', router)
