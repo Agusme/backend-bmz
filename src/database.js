e86a2aceb5e7867e27ec9aeee33d5318e7e6c505
agusmena <agusmena60@mail.com>
2024-05-21 18:52:21 -0300

    ad

--- src/database.js
+++ src/database.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose'
 import 'dotenv/config'
-
+/* 
 const url = 'mongodb://127.0.0.1:27017/bmzinmobiliaria';
 mongoose.connect(url) 
-
-/* const URI= process.env.MONGODB_URI_BMZ
-mongoose.connect(URI) */
-
+ */
+const URI= process.env.MONGODB_URI_BMZ
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
 const connection = mongoose.connection
 connection.once('open', ()=>{
-    console.log(" bd conectada")
+    console.log("bd conectada")
 })
